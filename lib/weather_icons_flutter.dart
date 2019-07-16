85cc222f3d43ccb0cb2d9795c52289250b85119b
Rishi Banerjee <rishieric91@gmail.com>
2019-07-16 08:00:04 +0530

    Completed the DayTime Set

--- lib/weather_icons_flutter.dart
+++ lib/weather_icons_flutter.dart
@@ -4,5 +4,55 @@ import 'package:flutter/widgets.dart';
 import 'package:weather_icons_flutter/icon_data.dart';
 
 class WeatherIcons {
+
+  /// Daytime
   static const IconData daySunny = const WeatherIconData(0xf00d);
+  static const IconData dayCloudy = const WeatherIconData(0xf00d);
+  static const IconData dayCloudyGusts = const WeatherIconData(0xf000);
+  static const IconData dayCloudyWindy = const WeatherIconData(0xf001);
+  static const IconData dayFog = const WeatherIconData(0xf003);
+  static const IconData dayHail = const WeatherIconData(0xf004);
+  static const IconData dayHaze = const WeatherIconData(0xf0b6);
+  static const IconData dayLightning = const WeatherIconData(0xf005);
+  static const IconData dayRain = const WeatherIconData(0xf008);
+  static const IconData dayRainMix = const WeatherIconData(0xf006);
+  static const IconData dayRainWind = const WeatherIconData(0xf007);
+  static const IconData dayShowers = const WeatherIconData(0xf009);
+  static const IconData daySleet = const WeatherIconData(0xf0b2);
+  static const IconData daySleetStorm = const WeatherIconData(0xf068);
+  static const IconData daySnow = const WeatherIconData(0xf00a);
+  static const IconData daySnowThunderstorm = const WeatherIconData(0xf06b);
+  static const IconData daySnowWind = const WeatherIconData(0xf065);
+  static const IconData dayDaySprinkle = const WeatherIconData(0xf00b);
+  static const IconData dayStormShowers = const WeatherIconData(0xf00e);
+  static const IconData daySunnyOvercast = const WeatherIconData(0xf00c);
+  static const IconData dayThunderstorm = const WeatherIconData(0xf010);
+  static const IconData dayWindy = const WeatherIconData(0xf085);
+  static const IconData daySolarEclipse = const WeatherIconData(0xf06e);
+  static const IconData dayHot = const WeatherIconData(0xf072);
+  static const IconData dayCloudyHigh = const WeatherIconData(0x7d);
+  static const IconData dayLightWind = const WeatherIconData(0xf0c4);
+
+  /// Nighttime
+  /// TODO
+  /// Neutral
+  /// TODO
+  /// Miscellaneous
+  /// TODO
+  /// Moon Phases
+  /// TODO
+  /// Time
+  /// TODO
+  /// Directional Arrows
+  /// TODO
+  /// Wind Degree Examples
+  /// TODO
+  /// Wind Cardinal Examples
+  /// TODO
+  /// Beaufort Wind Scale
+  /// TODO
+
+
+
+
 }
